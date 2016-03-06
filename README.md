1550070f058678d43e31ce16f789490db522a1b5
RongYu98 <Stuy.rong.yu@gmail.com>
2016-03-06 12:53:12 -0500

    player movement is done, but not shooting

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Asteroids Remake:
 - Player: inital location at the center of the screen, moves with arrow keys, shoots with mouseclick or space. Upon collision with an asteroid, dies.
 - Player Movement Mechanics: Left/Right arrow keys rotates it in its position( xy shouldn't change ), Up/Down propel it forwards/backwards at its angle. 
 - Asteroids: move with velocity depending on size (larger:slower->smaller:faster). Upon being shot, fractures into two smaller asteroids of a smaller size. Three size tiers. 
+
+Notes About Player Movement Mechanics:
+- All angles are in RADIANS
+- The 0 radian mark shall be the positive x axis, going COUNTER-clockwise
--- animate.js
+++ animate.js
@@ -2,25 +2,25 @@ var c= document.getElementById("playground");
 var ctx = c.getContext("2d");
 var requestID;
 
-var logo = new Image();
-logo.src = "logo_dvd.jpg";
-
 var desist = function HALT(){
     window.cancelAnimationFrame( requestID );
 }    
 
 var stop = document.getElementById("stop");
 stop.addEventListener( "click", desist);
+
 //for the enemy circle
 var dx = 1;
 var dy = 3;
 var x = c.width/3*2;
 var y = c.height/3*2;
+
 //usx = us's x
 var usx = 100;
 var usy = 100;
-var usdx = 0;
-var usdy = 0;
+var v = 0;
+var angle = 0;
+
 //mx = mouse x
 var mx = 0;
 var my = 0;
@@ -38,46 +38,49 @@ var bounce = function(){
     y += dy;
 
     ctx.beginPath();
+    ctx.fillStyle = "#000000";
     ctx.arc( x, y, 10, 0, 2 * Math.PI );
     ctx.stroke();
     ctx.fill();
     
-    //movement for the player
-    //var angle = Math.atan( (y-my)/(x - mx) );
-    //console.log(angle);    
-    if (mx > x){
-        usdx = 1;
-    } else { usdx = -1; }
-    if (my > y ){ 
-       usdy = 1; } else { usdy = -1; }
-    usx += usdx;
-    usy += usdy;
-    
-
-
-    /* //requires more work with the math....
-    if ( mx > x ){
-        usdx = 1 * Math.sin(angle);
-        usdy = 1 * Math.cos(angle);
-    } else {
-        usdx = -1 * Math.sin(angle);
-        usdy = -1 * Math.cos(angle);
+    ////
+    if (v>5){
+        v = 5;
     }
-	*/
 
+    usx += v * Math.cos(angle);
+    usy += v * Math.sin(angle);
+    if (v>0){
+        v-=.005;
+    }
     ctx.beginPath();
-    ctx.arc( usx, usy, 10, 0, 2 * Math.PI );
+    ctx.fillStyle = "#0000ff";   
+    ctx.arc( usx, usy, 10, .25*Math.PI +angle, 1.75 * Math.PI + angle );    
     ctx.stroke();
     ctx.fill();
-
 
     requestID = window.requestAnimationFrame( bounce );
 }
 
+window.addEventListener("keydown", function(e){ //note angle is countercllockwise
+   //e.keyCode; 39 == right; 38 == up; 37 == left; 40 == down
+   //console.log(e.keyCode);
+   if ( e.keyCode == 39 ){
+       angle += .17; //right //radians
+   } if ( e.keyCode == 38 ){ //up
+       v += .1;
+   } if ( e.keyCode == 37 ){
+       angle -= .17;
+   } if ( e.keyCode == 40 ){
+       v -= .1;
+   }
+});
+
+// will be changed to shooting
 window.addEventListener('mousemove', function(e){
     mx = e.pageX;
     my = e.pageY;
-    console.log("x is "+mx+" y is "+my);
+    //console.log("x is "+mx+" y is "+my);
 });
 
 
