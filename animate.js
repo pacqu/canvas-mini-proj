4a3f97dca13e60820f054dc29b33e95165b0491e
RongYu98 <Stuy.rong.yu@gmail.com>
2016-03-06 18:21:45 -0500

    got the shooting and dying to work

--- animate.js
+++ animate.js
@@ -10,16 +10,20 @@ var stop = document.getElementById("stop");
 stop.addEventListener( "click", desist);
 
 //for the enemy circle
-var dx = 1;
-var dy = 3;
-var x = c.width/3*2;
-var y = c.height/3*2;
+var ex = c.width/3*2;
+var ey = c.height/3*2;
+var alive = true;
 
 //usx = us's x
 var usx = c.width/2;
 var usy = c.height/2;
 var v = 0;
 var angle = 0;
+var ualive = true;
+
+//shooting;
+var reloaded = true;
+var bx,by,bangle,bv;
 
 function asteroid(size, asx, asy, v, angle) {
     this.size = size;
@@ -46,6 +50,8 @@ function asteroid(size, asx, asy, v, angle) {
         this.asy = c.height;
       this.asx += v * Math.cos(this.angle);
       this.asy += v * Math.sin(this.angle);
+      ex = this.asx;
+      ey = this.asy;
     }
 }
 
@@ -59,30 +65,17 @@ var player = document.getElementById("dvd");
 var bounce = function(){
     ctx.clearRect( 0, 0, c.width, c.height );
     a1.move(ctx);
-    a1.draw(ctx);
-    /*
-    if ( ((x + 10) > c.width) || ( x < c.width/2 ) ){
-        dx *= -1;
-    } 
-    if ( ((y+10) > c.height) || (y -10 < 0)){
-        dy *= -1;
+    if (alive){
+        a1.draw(ctx);
     }
-    x += dx;
-    y += dy;
-
-    ctx.beginPath();
-    ctx.fillStyle = "#000000";
-    ctx.arc( x, y, 10, 0, 2 * Math.PI );
-    ctx.stroke();
-    ctx.fill();
-    */
-
-
-    ////
+ 
+    if ( (usx-ex)*(usx-ex) + (usy-ey)*(usy-ey) < 1210 ) {
+        ualive = false;
+    }
     if (v>5){
         v = 5;}
     if (usx < 0){
-        usx = c.wdith;} 
+        usx = c.width;} 
     if (usy < 0){
         usy = c.height;}
     if (usx > c.width){
@@ -92,16 +85,47 @@ var bounce = function(){
     usx += v * Math.cos(angle);
     usy += v * Math.sin(angle);
     if (v>0){
-        v-=.005;
+        //v-=.005;
     }
-    ctx.beginPath();
-    ctx.fillStyle = "#0000ff";   
-    ctx.arc( usx, usy, 10, .25*Math.PI +angle, 1.75 * Math.PI + angle );    
-    ctx.stroke();
-    ctx.fill();
+    if (ualive){
+        ctx.beginPath();
+        ctx.fillStyle = "#0000ff";   
+        ctx.arc( usx, usy, 10, .25*Math.PI +angle, 1.75 * Math.PI + angle );    
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
 
+    //bullet
+    if (!reloaded){ //i.e. still flying
+        bx += Math.cos(bangle) * bv;
+        by += Math.sin(bangle) * bv;
+        ctx.beginPath();
+        ctx.fillStyle = "#000080";   
+        ctx.arc(bx, by, 10 , 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+        if ( (bx > c.width) || bx < 0 || by > c.height || by < 0){
+            reloaded = true;
+            console.log(true);
+        }
+        if ( (bx - ex)*(bx-ex) + (by-ey)*(by-ey) < 1210 ){
+            alive = false;
+            reloaded = true;
+        }
+    }
     requestID = window.requestAnimationFrame( bounce );
 }
+
+function shoot(){
+    if (reloaded && ualive){
+        bx = usx;
+        by = usy;
+        bv = 5 +v;
+        bangle = angle;
+        reloaded = false;
+    }
+};
 
 window.addEventListener("keydown", function(e){ //note angle is countercllockwise
    //e.keyCode; 39 == right; 38 == up; 37 == left; 40 == down
@@ -114,15 +138,9 @@ window.addEventListener("keydown", function(e){ //note angle is countercllockwis
        angle -= .17;
    } if ( e.keyCode == 40 ){
        v -= .1;
+   } if ( e.keyCode == 70 || e.keyCode == 32){ //f or space
+       shoot();
    }
 });
-
-// will be changed to shooting
-window.addEventListener('mousemove', function(e){
-    mx = e.pageX;
-    my = e.pageY;
-    //console.log("x is "+mx+" y is "+my);
-});
-
 
 player.addEventListener( "click", bounce )
